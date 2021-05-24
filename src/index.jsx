c5dd7078b0005be6aa72cd0b8277605c48387d1a
Andrew <andrflett@gmail.com>
2021-05-24 13:22:33 +0100

    WIP

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,12 @@ import Theme from './components/Theme'
 import ChunkyButton from './components/ChunkyButton'
 import ChakraAwesome from './components/ChakraAwesome'
 import ActionButton from './components/ActionButton'
+import PrimaryButton from './components/PrimaryButton'
 
 export {
   ActionButton,
   ChunkyButton,
   ChakraAwesome,
+  PrimaryButton,
   Theme
 }
