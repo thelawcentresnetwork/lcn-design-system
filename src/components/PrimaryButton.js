ca8c0e9eefac727bb8f712ca5bb7ee7defc7649f
Andrew <andrflett@gmail.com>
2021-05-24 13:50:41 +0100

    Buttons

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -6,23 +6,19 @@ function PrimaryButton(props) {
 
   return (
     <Button
-      {...props}
       as="a"
       borderRadius="lg"
       fontSize="0.85rem"
       color="white"
       bg="green.700"
       cursor="pointer"
-      borderLeftWidth="1px"
-      borderRightWidth="3px"
-      borderBottomWidth="3px"
-      borderColor="brand.Green"
       _hover={{ bg: "brand.Green" }}
       _focus={{ outline: 0 }}
       _active={{ outline: 0 }}
       px="5"
       pt="5"
-      pb="1.2rem">
+      pb="1.2rem"
+      {...props}>
       { props.icon &&
         <ChakraAwesome color="white" mr='4' icon={['fas', props.icon]} />
       }
