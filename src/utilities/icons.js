03a3a46d1a0ae1a5d622dfcadb358c8611a9e6a8
Andrew <andrflett@gmail.com>
2021-06-09 10:36:20 +0100

    WIP

--- src/utilities/icons.js
+++ src/utilities/icons.js
@@ -1,17 +1,18 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false;
 
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(
-  fal,
-  fas,
-  far,
-  fab
-)
+// These should be imported directly in your project, not here
+// import { fal } from '@fortawesome/pro-light-svg-icons'
+// import { far } from '@fortawesome/pro-regular-svg-icons'
+// import { fas } from '@fortawesome/pro-solid-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+//
+// library.add(
+//   fal,
+//   fas,
+//   far,
+//   fab
+// )
 
 const faLCNLogo = {
   prefix: 'fak',
