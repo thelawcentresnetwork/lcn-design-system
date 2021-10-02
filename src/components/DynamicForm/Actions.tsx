ad3e0be87dd05ae754255268b389f8ea88ba3a8c
Andrew Flett <andrflett@gmail.com>
2021-10-02 10:03:19 +0100

    WIP

--- src/components/DynamicForm/Actions.tsx
+++ src/components/DynamicForm/Actions.tsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { useFormContext } from "react-hook-form";
-//@ts-ignore
-import ChakraAwesome from '../../utilities/ChakraAwesome';
 
 import {
     Button,
@@ -14,7 +11,7 @@ interface DActionsProps extends StackProps {
         submit: string,
         cancel: string
     }
-    onCancel: () => {}
+    onCancel?: () => {}
 }
 
 export default function DActions({
@@ -27,11 +24,13 @@ export default function DActions({
 
         <HStack w="100%" spacing="5" {...boxProps}>
             <Button type="submit">{labels?.submit || "Submit"}</Button>
-            <Button
-                onClick={onCancel}
-                variant="outline"
-                colorScheme="gray">
-                {labels?.cancel || "Cancel"}</Button>
+            {!onCancel &&
+                <Button
+                    onClick={onCancel}
+                    variant="outline"
+                    colorScheme="gray">
+                    {labels?.cancel || "Cancel"}</Button>
+            }
         </HStack>
     )
 
