a288a10e1b530c7f6fe96a37b7c3ab0e522da36e
Andrew Flett <andrflett@gmail.com>
2021-12-01 13:09:38 +0000

    WIP

--- src/components/DynamicForm/Actions.tsx
+++ src/components/DynamicForm/Actions.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import {
     Button,
+    ButtonProps,
     StackProps,
     HStack
 } from '@chakra-ui/react'
 
 interface DActionsProps extends StackProps {
+    buttonProps?: {
+        submit: ButtonProps,
+        cancel: ButtonProps
+    },
     labels?: {
         submit: string,
         cancel: string
@@ -17,18 +22,24 @@ interface DActionsProps extends StackProps {
 export default function DActions({
     labels,
     onCancel,
+    buttonProps,
     ...boxProps
 }: DActionsProps) {
 
     return (
 
         <HStack w="100%" spacing="5" {...boxProps}>
-            <Button type="submit">{labels?.submit || "Submit"}</Button>
+            <Button
+                {...buttonProps?.submit}
+                type="submit">
+                {labels?.submit || "Submit"}
+            </Button>
             {onCancel &&
                 <Button
                     onClick={onCancel}
                     variant="outline"
-                    colorScheme="gray">
+                    colorScheme="gray"
+                    {...buttonProps?.cancel}>
                     {labels?.cancel || "Cancel"}</Button>
             }
         </HStack>
