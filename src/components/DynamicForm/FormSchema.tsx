225869911d6696ff0c7d1615832727271ea719e3
Andrew Flett <andrflett@gmail.com>
2022-04-04 23:24:58 +0100

    Fix broken form input names

--- src/components/DynamicForm/FormSchema.tsx
+++ src/components/DynamicForm/FormSchema.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 import { UseFormReturn } from "react-hook-form";
 
-import {
-    Stack
-} from '@chakra-ui/react'
+import { Stack } from "@chakra-ui/react";
 
 import DInput from "./Input";
 import DNumber from "./Number";
@@ -16,151 +14,149 @@ import DDate from "./Date";
 import DFile from "./File";
 
 interface DFormProps {
-    useForm: UseFormReturn,
-    schema: {
-        properties: {
-            name: string,
-            type: string,
-            label?: string,
-            hint?: string,
-            items?: {
-                value: string,
-                label: string
-            }[],
-            ui?: string,
-            placeholder?: string,
-            leftAddon?: string,
-            rightAddon?: string,
-            validation?: {}
-        }[]
-    }
+  useForm: UseFormReturn;
+  schema: {
+    properties: {
+      name: string;
+      type: string;
+      label?: string;
+      hint?: string;
+      items?: {
+        value: string;
+        label: string;
+      }[];
+      ui?: string;
+      ppi?: boolean;
+      placeholder?: string;
+      leftAddon?: string;
+      rightAddon?: string;
+      validation?: {};
+    }[];
+  };
 }
 
 interface DFormFieldProps {
-    input: string
+  input: string;
 }
 
 export default function DFormSchema({ schema, useForm }: DFormProps) {
+  const FormField = ({ input }: DFormFieldProps) => {
+    let field = schema.properties.find((p) => p.name === input);
 
-    const FormField = ({ input }: DFormFieldProps) => {
+    if (field) {
+      let { validation, type, items, ui, name, ppi, ...inputProps } = field;
 
-        let field = schema.properties.find(p => p.name === input)
+      switch (type) {
+        case "string":
+          return (
+            <DInput
+              useForm={useForm}
+              name={input}
+              validation={validation}
+              {...inputProps}
+            />
+          );
+        case "integer":
+          return (
+            <DNumber
+              name={input}
+              useForm={useForm}
+              validation={validation}
+              {...inputProps}
+            />
+          );
+        case "date":
+          return (
+            <DDate
+              useForm={useForm}
+              name={input}
+              validation={validation}
+              {...inputProps}
+            />
+          );
+        case "boolean":
+          return (
+            <>
+              {ui == "switch" && (
+                <DCheckbox
+                  useForm={useForm}
+                  type="switch"
+                  name={input}
+                  validation={validation}
+                  {...inputProps}
+                />
+              )}
+              {!ui && (
+                <DCheckbox
+                  useForm={useForm}
+                  name={input}
+                  validation={validation}
+                  {...inputProps}
+                />
+              )}
+            </>
+          );
 
-        if (field) {
-
-            let { validation, type, items, ui, name, ...inputProps } = field
-
-            switch (type) {
-                case "string":
-                    return (
-                        <DInput
-                            useForm={useForm}
-                            name={input}
-                            validation={validation}
-                            {...inputProps} />
-                    )
-                case "integer":
-                    return (
-                        <DNumber
-                            name={input}
-                            useForm={useForm}
-                            validation={validation}
-                            {...inputProps} />
-                    )
-                case "date":
-                    return (
-                        <DDate
-                            useForm={useForm}
-                            name={input}
-                            validation={validation}
-                            {...inputProps} />
-                    )
-                case "boolean":
-                    return (
-                        <>
-                            {
-                                ui == "switch" &&
-                                <DCheckbox
-                                    useForm={useForm}
-                                    type='switch'
-                                    name={input}
-                                    validation={validation}
-                                    {...inputProps} />
-                            }
-                            {
-                                !ui &&
-                                <DCheckbox
-                                    useForm={useForm}
-                                    name={input}
-                                    validation={validation}
-                                    {...inputProps} />
-                            }
-                        </>
-                    )
-
-                case "array":
-                    return (
-                        <>
-                            {
-                                field.ui == "radio" &&
-                                <DRadio
-                                    useForm={useForm}
-                                    options={items}
-                                    name={input}
-                                    validation={validation}
-                                    {...inputProps} />
-                            }
-                            {
-                                !field.ui &&
-                                <DSelect
-                                    useForm={useForm}
-                                    options={items}
-                                    name={input}
-                                    validation={validation}
-                                    {...inputProps} />
-                            }
-                        </>
-                    )
-                case "text":
-                    return (
-                        <DTextarea
-                            useForm={useForm}
-                            name={input}
-                            validation={validation}
-                            {...inputProps} />
-                    )
-                case "file":
-                    return (
-                        <DFile
-                            useForm={useForm}
-                            name={input}
-                            validation={validation}
-                            {...inputProps} />
-                    )
-                default:
-                    console.log("Can not render field: " + input);
-                    return (<></>);
-            }
-        } else {
-            console.log("Can not render field: " + input);
-            return (<></>);
-        }
-
+        case "array":
+          return (
+            <>
+              {field.ui == "radio" && (
+                <DRadio
+                  useForm={useForm}
+                  options={items}
+                  name={input}
+                  validation={validation}
+                  {...inputProps}
+                />
+              )}
+              {!field.ui && (
+                <DSelect
+                  useForm={useForm}
+                  options={items}
+                  name={input}
+                  validation={validation}
+                  {...inputProps}
+                />
+              )}
+            </>
+          );
+        case "text":
+          return (
+            <DTextarea
+              useForm={useForm}
+              name={input}
+              validation={validation}
+              {...inputProps}
+            />
+          );
+        case "file":
+          return (
+            <DFile
+              useForm={useForm}
+              name={input}
+              validation={validation}
+              {...inputProps}
+            />
+          );
+        default:
+          console.log("Can not render field: " + input);
+          return <></>;
+      }
+    } else {
+      console.log("Can not render field: " + input);
+      return <></>;
     }
+  };
 
-    return (
-        <>
-            {
-                schema.properties &&
-                <Stack spacing="6">
-
-                    {schema.properties?.map((property, index) => (
-                        <FormField key={index} input={property.name} />
-                    ))}
-
-                </Stack>
-            }
-        </>
-    )
-
+  return (
+    <>
+      {schema.properties && (
+        <Stack spacing="6">
+          {schema.properties?.map((property, index) => (
+            <FormField key={index} input={property.name} />
+          ))}
+        </Stack>
+      )}
+    </>
+  );
 }
--- src/patterns/FormBuilder/FormBuilder.jsx
+++ src/patterns/FormBuilder/FormBuilder.jsx
@@ -1,175 +1,168 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import parameterize from "parameterize-js";
 
 import { useForm, FormProvider, useFieldArray } from "react-hook-form";
 
 import {
-    Button,
-    Stack,
-    Heading,
-    Table,
-    Tr,
-    Th,
-    Thead,
-    Tbody,
-    Box,
-} from "@chakra-ui/react"
+  Button,
+  Stack,
+  Heading,
+  Table,
+  Tr,
+  Th,
+  Thead,
+  Tbody,
+  Box,
+} from "@chakra-ui/react";
 
-import Field from './Field'
+import Field from "./Field";
 
-import ChakraAwesome from '../../utilities/ChakraAwesome';
-import DActions from '../../components/DynamicForm/Actions';
+import ChakraAwesome from "../../utilities/ChakraAwesome";
+import DActions from "../../components/DynamicForm/Actions";
 
 const fieldTypes = [
-    {
-        value: 'string',
-        label: 'Short Text'
-    },
-    {
-        value: 'text',
-        label: 'Long text'
-    },
-    {
-        value: 'integer',
-        label: 'Number'
-    },
-    {
-        value: 'array',
-        label: 'Dropdown'
-    },
-    {
-        value: 'boolean',
-        label: 'Checkbox'
-    },
-    {
-        value: 'date',
-        label: 'Date'
-    }
-]
+  {
+    value: "string",
+    label: "Short Text",
+  },
+  {
+    value: "text",
+    label: "Long text",
+  },
+  {
+    value: "integer",
+    label: "Number",
+  },
+  {
+    value: "array",
+    label: "Dropdown",
+  },
+  {
+    value: "boolean",
+    label: "Checkbox",
+  },
+  {
+    value: "date",
+    label: "Date",
+  },
+];
 
 export default function FormBuilder({ schema, onCancel, onSave, labels }) {
+  const formMethods = useForm();
+  const {
+    reset,
+    formState: { errors },
+  } = formMethods;
 
-    const formMethods = useForm();
-    const { reset, formState: { errors } } = formMethods
+  const {
+    fields: fields,
+    swap: swapField,
+    append: appendField,
+    remove: removeField,
+  } = useFieldArray({
+    control: formMethods.control,
+    name: "fields",
+  });
 
-    const {
-        fields: fields,
-        swap: swapField,
-        append: appendField,
-        remove: removeField } = useFieldArray({
-            control: formMethods.control,
-            name: "fields"
-        });
+  useEffect(() => {
+    if (schema?.properties) {
+      const schemaFields = schema?.properties?.map((field, index) => {
+        return {
+          name: field.name,
+          id: index,
+          label: field.label,
+          type: field.type,
+          items: field.items?.map((item) => item.value),
+          ppi: field.ppi,
+          required: field.validation?.hasOwnProperty("required"),
+        };
+      });
 
-    useEffect(() => {
-
-        if (schema?.properties) {
-
-            const schemaFields = schema?.properties?.map((field, index) => {
-                return {
-                    name: field.name,
-                    id: index,
-                    label: field.label,
-                    type: field.type,
-                    items: field.items?.map((item) => item.value),
-                    ppi: field.ppi,
-                    required: field.validation?.hasOwnProperty('required')
-                }
-            })
-
-            reset({ fields: schemaFields })
-
-        }
-
-    }, [schema])
-
-    const onSubmit = (values) => {
-
-        let newSchema = schema
-        newSchema.properties = []
-
-        values.fields.forEach((field, index) => {
-
-            let newField = {
-                name: field.label,
-                label: field.label,
-                type: field.type,
-                ppi: field.ppi,
-                items: field.items?.map((item) => ({ value: item, label: item }))
-            }
-
-            if (field.required) {
-                newField['validation'] = { required: 'Please fill in this field.' }
-            }
-
-            newSchema.properties.push(newField)
-
-        })
-
-        onSave(newSchema)
-
+      reset({ fields: schemaFields });
     }
+  }, [schema]);
 
-    return (
+  const onSubmit = (values) => {
+    let newSchema = schema;
+    newSchema.properties = [];
 
+    values.fields.forEach((field, index) => {
+      let newField = {
+        name: parameterize(field.label),
+        label: field.label,
+        type: field.type,
+        ppi: field.ppi,
+        items: field.items?.map((item) => ({ value: item, label: item })),
+      };
+
+      if (field.required) {
+        newField["validation"] = { required: "Please fill in this field." };
+      }
+
+      newSchema.properties.push(newField);
+    });
+
+    onSave(newSchema);
+  };
+
+  return (
+    <>
+      {schema && (
         <>
-            {schema &&
-                <>
-                    <FormProvider {...formMethods}>
-                        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
-                            <Box pb="5" px="0" mt="-8">
-                                <Stack spacing="5">
+          <FormProvider {...formMethods}>
+            <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+              <Box pb="5" px="0" mt="-8">
+                <Stack spacing="5">
+                  {fields && (
+                    <Table variant="simple">
+                      <Thead>
+                        <Tr>
+                          <Th borderBottom="0"></Th>
+                          <Th borderBottom="0"></Th>
+                          <Th borderBottom="0"></Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {fields.map((field, index) => (
+                          <Field
+                            fieldTypes={fieldTypes}
+                            key={field.id}
+                            index={index}
+                            total={fields.length}
+                            field={field}
+                            swapField={swapField}
+                            removeField={removeField}
+                          />
+                        ))}
+                      </Tbody>
+                    </Table>
+                  )}
 
-                                    {fields &&
-                                        <Table variant="simple">
-                                            <Thead>
-                                                <Tr>
-                                                    <Th borderBottom="0"></Th>
-                                                    <Th borderBottom="0"></Th>
-                                                    <Th borderBottom="0"></Th>
-                                                </Tr>
-                                            </Thead>
-                                            <Tbody>
+                  <Button
+                    variant="inverse"
+                    leftIcon={<ChakraAwesome icon="plus" />}
+                    onClick={(e) => appendField({ type: "string" })}
+                  >
+                    Add field
+                  </Button>
+                </Stack>
+              </Box>
 
-                                                {
-                                                    fields.map((field, index) => (
-                                                        <Field
-                                                            fieldTypes={fieldTypes}
-                                                            key={field.id}
-                                                            index={index}
-                                                            total={fields.length}
-                                                            field={field}
-                                                            swapField={swapField}
-                                                            removeField={removeField} />
-                                                    ))
-                                                }
-
-                                            </Tbody>
-                                        </Table>
-                                    }
-
-                                    <Button
-                                        variant="inverse"
-                                        leftIcon={<ChakraAwesome icon="plus" />}
-                                        onClick={e => appendField({ type: "string" })}>Add field</Button>
-
-                                </Stack>
-                            </Box>
-
-                            <Box
-                                pt="3"
-                                justifyContent="flex-start">
-                                <DActions
-                                    labels={labels || {
-                                        submit: 'Save Form',
-                                        cancel: 'Cancel'
-                                    }}
-                                    onCancel={onCancel} />
-                            </Box>
-                        </form>
-                    </FormProvider>
-                </>
-            }
+              <Box pt="3" justifyContent="flex-start">
+                <DActions
+                  labels={
+                    labels || {
+                      submit: "Save Form",
+                      cancel: "Cancel",
+                    }
+                  }
+                  onCancel={onCancel}
+                />
+              </Box>
+            </form>
+          </FormProvider>
         </>
-    )
-
+      )}
+    </>
+  );
 }
