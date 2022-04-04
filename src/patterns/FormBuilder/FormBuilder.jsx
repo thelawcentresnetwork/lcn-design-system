8e91b75ec0fbe25d3600ea1d37b0f97f17a4d88b
Andrew Flett <andrflett@gmail.com>
2022-04-05 00:52:14 +0100

    WIP

--- src/patterns/FormBuilder/FormBuilder.jsx
+++ src/patterns/FormBuilder/FormBuilder.jsx
@@ -89,7 +89,7 @@ export default function FormBuilder({ schema, onCancel, onSave, labels }) {
     values.fields.forEach((field, index) => {
       let newField = {
         //name: parameterize(field.label),
-        name: field.label?.replaceAll("'", ""),
+        name: field.label?.replaceAll("'", "")?.replaceAll(".", ""),
         label: field.label,
         type: field.type,
         ppi: field.ppi,
