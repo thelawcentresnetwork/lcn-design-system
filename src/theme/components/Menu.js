9cf31a820965ba227bee3ca1d9b00c547b10fb5f
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-06-27 11:02:19 +0100

    feat: add menu component

--- src/theme/components/Menu.js
+++ src/theme/components/Menu.js
@@ -1,27 +1,54 @@
 const Menu = {
-  baseStyle: ({ colorScheme }) => ({
+  baseStyle: () => ({
+    button: {
+      fontFamily: 'Anek Latin, sans-serif',
+      textTransform: 'uppercase',
+      color: 'brand.darkBlue',
+    },
     list: {
       py: '0',
       px: '3',
+      padding: '1rem 1rem 0.5rem 1rem',
+      borderRadius: 'none',
+      fontFamily: 'Anek Latin, sans-serif',
+      bg: 'brand.warmYellow',
+      color: 'brand.darkBlue',
+      position: 'relative',
+      transform: 'translateX(-12%) !important',
+
+      _after: {
+        content: '""',
+        position: 'absolute',
+        bg: 'brand.warmYellow',
+        height: '42px',
+        width: '80%',
+        left: '0px',
+        bottom: -10,
+      },
     },
     item: {
-      fontSize: 'sm',
-      color: 'gray.600',
-      borderRadius: 'md',
-      _hover: { bg: `${colorScheme}.50`, color: `${colorScheme}.500` },
-      _active: { bg: `${colorScheme}.50` },
-      _focus: { bg: `${colorScheme}.50` },
-      _first: { mt: '3' },
-      _last: { mb: '3' },
+      fontSize: '4xs',
+      bg: 'brand.warmYellow',
+      lineHeight: '1.2rem',
+      maxWidth: '16ch',
+      textWrap: 'wrap',
+      _hover: { textDecoration: `underline` },
+      _active: { textDecoration: `underline` },
+      _focus: { textDecoration: `underline` },
+    },
+
+    divider: {
+      borderColor: 'brand.darkBlue',
+      width: '80%',
     },
     groupTitle: {
       _first: { mt: '3' },
       mx: '3',
+      fontSize: '4xs',
+      textDecoration: 'underline',
+      paddingBottom: 0,
     },
   }),
-  defaultProps: {
-    colorScheme: 'green',
-  },
 }
 
 export default Menu
