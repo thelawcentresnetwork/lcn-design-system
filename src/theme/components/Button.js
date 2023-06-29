c8603bb668757020f27732a1b0ff35d73bf1d02f
roseb.04 <rose@neontribe.co.uk>
2023-06-29 15:21:22 +0100

    refactor: button theme style

--- src/theme/components/Button.js
+++ src/theme/components/Button.js
@@ -1,50 +1,60 @@
-const Button = {
-  sizes: {
-    long: {
-      minWidth: '30rem',
-      py: '0.5rem',
-    },
-    short: {
-      minWidth: '15rem',
-      py: '0.5rem',
-    },
-  },
-  variants: {
-    solid: ({ colorScheme }) => ({
-      borderRadius: 'none',
-      textTransform: 'uppercase',
-      fontFamily: 'bodyAlternative',
-      fontSize: '2xs',
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+
+const baseStyle = defineStyle({
+  borderRadius: 0,
+  fontFamily: 'bodyAlternative',
+  fontSize: '2xs',
+  textTransform: 'uppercase',
+})
+
+const solid = defineStyle(({ colorScheme }) => {
+  return {
+    bg: `${colorScheme}.500`,
+    color: `${colorScheme}.accessible`,
+    _hover: { bg: `${colorScheme}.400` },
+    _focus: { bg: `${colorScheme}.400` },
+    _active: { bg: `${colorScheme}.400` },
+    _focusVisible: { bg: `${colorScheme}.400` },
+  }
+})
+
+const outline = defineStyle(({ colorScheme }) => {
+  return {
+    border: '2px solid',
+    borderColor: `${colorScheme}.500`,
+    bg: 'none',
+    color: `${colorScheme}.500`,
+    _hover: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
+    _focus: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
+    _active: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
+    _focusVisible: {
       bg: `${colorScheme}.500`,
       color: `${colorScheme}.accessible`,
-      _hover: { bg: `${colorScheme}.400` },
-      _focus: { bg: `${colorScheme}.400` },
-      _active: { bg: `${colorScheme}.400` },
-      _focusVisible: { bg: `${colorScheme}.400` },
-    }),
-    outline: ({ colorScheme }) => ({
-      borderRadius: 'none',
-      textTransform: 'uppercase',
-      fontFamily: 'bodyAlternative',
-      border: '2px solid',
-      borderColor: `${colorScheme}.500`,
-      bg: 'none',
-      fontSize: '2xs',
-      color: `${colorScheme}.500`,
-      _hover: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
-      _focus: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
-      _active: { bg: `${colorScheme}.500`, color: `${colorScheme}.accessible` },
-      _focusVisible: {
-        bg: `${colorScheme}.500`,
-        color: `${colorScheme}.accessible`,
-      },
-    }),
-  },
-  defaultProps: {
-    variant: 'solid',
-    size: 'long',
-    colorScheme: 'brand.warmYellow',
-  },
+    },
+  }
+})
+
+const sizes = {
+  long: defineStyle({
+    minWidth: '30rem',
+    py: '0.5rem',
+  }),
+  short: defineStyle({
+    minWidth: '15rem',
+    py: '0.5rem',
+  }),
 }
 
-export default Button
+export const Button = defineStyleConfig({
+  baseStyle,
+  sizes,
+  variants: {
+    solid,
+    outline,
+  },
+  defaultProps: {
+    colorScheme: 'brand.warmYellow',
+    variant: 'solid',
+    size: 'long',
+  },
+})
