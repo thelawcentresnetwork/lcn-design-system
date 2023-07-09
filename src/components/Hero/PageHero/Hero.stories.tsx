e4327b9bcd9f248dcb582144ee0935e9b3fbda90
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-07-10 00:29:38 +0100

    feat: add dropdown option for colours

--- src/components/Hero/PageHero/Hero.stories.tsx
+++ src/components/Hero/PageHero/Hero.stories.tsx
@@ -1,6 +1,38 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Hero from './Hero'
 import React from 'react'
+import colors from '../../../theme/foundations/colors'
+
+const newColors = colors.brand
+
+// Creates an array of the colour codes, setting colours with objects as values to be a string for the colour code of 500
+
+const colorValuesArray = () => {
+  Object.keys(newColors).forEach((key: string) => {
+    const currentValue = newColors[key as keyof typeof newColors]
+    if (typeof currentValue !== 'string') {
+      newColors[key as keyof typeof newColors] = currentValue['500']
+    }
+  })
+
+  return Object.values(newColors)
+}
+
+// Creates the object where they keys and values are reversed to generate labels. See these docs https://storybook.js.org/docs/react/essentials/controls
+
+const newColourObject = Object.fromEntries(
+  Object.entries(newColors).map(([key, value]) => [value, key])
+)
+
+// generates the argType for textColour and backgroundColour
+
+const colourArgType = {
+  options: colorValuesArray(),
+  control: {
+    type: 'select',
+    labels: newColourObject,
+  },
+}
 
 const meta: Meta = {
   component: Hero,
@@ -19,6 +51,10 @@ export const Default: Story = {
     heading: 'Fairness is at the heart of what we do',
     subHeading:
       'Lorem ipsum dolor sit amet consectetur. Sed hac laoreet risus iaculis donec. In dolor tincidunt sed elementum cursus rhoncus adipiscing. Semper sed eget dignissim.',
+  },
+  argTypes: {
+    backgroundColour: colourArgType,
+    textColour: colourArgType,
   },
   render: (args) => <Hero {...args} />,
 }
