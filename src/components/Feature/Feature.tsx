4f49c73299b552639d93e21c32d70f79793fc1ea
maxgerber <mgerber94@gmail.com>
2024-05-13 18:02:48 +0100

    * Conf2024 Feature ++ storybook (add fonts)

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -11,9 +11,10 @@ export interface FeatureProps {
   }[]
   textColor?: string
   centred?: boolean
+  conference2024?: boolean
 }
 
-const Feature: React.FC<FeatureProps> = ({ featureItems, textColor }) => (
+const Feature: React.FC<FeatureProps> = ({ featureItems, textColor, conference2024 }) => (
   <Box as="ul">
     {featureItems.map((featureItem) => (
       <Flex
@@ -28,15 +29,15 @@ const Feature: React.FC<FeatureProps> = ({ featureItems, textColor }) => (
         color={textColor}
       >
         <Text
-          fontFamily="bodyAlternative"
-          fontSize="lg"
+          fontFamily={conference2024 ? "headingConference2024" : "bodyAlternative"}
+          fontSize={conference2024 ? "h3Conference2024" : "lg"}
           lineHeight={1.2}
           width={{ base: '100%', sm: '250px' }}
         >
           {' '}
           {featureItem.fields.label}{' '}
         </Text>
-        <Text width={{ base: '100%', sm: '250px' }}>
+        <Text width={{ base: '100%', sm: '250px' }} {...(conference2024 && {fontFamily: "Archivo", fontSize: "18px", lineHeight: "26.784px"})}>
           {featureItem.fields.description}
         </Text>
       </Flex>
