08879b417c1cdaa2d12f78ab0ecb33ee2f0d8482
lorezzed <jjsalamon@gmail.com>
2025-04-29 19:17:09 +0100

    bump

--- docs/helpers/Provider.js
+++ docs/helpers/Provider.js
@@ -3,7 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Theme from '../../src/theme'
 import { library, fal, far, fas, fad, fab } from '../../src/polyfill'
 
-
 library.add(fal, fas, far, fad, fab)
 
 export default function Provider({ children }) {
